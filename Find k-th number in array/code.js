12d268ce8129ff7300c2bde4de3940708accee31
kyurim9399 <63507669+kyurim9399@users.noreply.github.com>
2020-06-05 01:19:28 +0900

    Add files via upload

---
+++ Find k-th number in array/code.js
@@ -0,0 +1,36 @@
+function solution(array, commands) {
+    var answer = [];
+    var valid = true;
+    
+    if(array.length>=1&& array.length<=100 && 
+        commands.length >=1 && commands.length<=100){
+        for(var i = 0; i<array.length; i++){
+            if(array[i]<1 || array>100){
+                valid = false;
+                break;
+            }
+        }
+        for(var i = 0; i < commands.lenght ; i++){
+            if(commands[i].lenght != 3){
+                valid = false;
+                break;
+            }
+        }
+    }
+    
+    if(valid){
+        for(var i = 0; i<commands.length; i++){
+            //자르기
+            var arr = array.slice(commands[i][0]-1,commands[i][1]);
+            //정렬
+            arr.sort(function(a,b){
+                return a-b;
+            })
+            //수찾기
+            answer.push(arr[commands[i][2]-1]);
+        }
+    }
+    return answer;
+}
+
+console.log(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]));
